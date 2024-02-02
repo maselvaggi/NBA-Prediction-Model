0393d48dc07796913902eae9b293d9381e006155
Matt Selvaggi <mselvagg@stevens.edu>
2024-02-02 01:34:56 -0500

    updated documentation

--- update_data.py
+++ update_data.py
@@ -6,14 +6,17 @@ from espn_game_info import *
 from seasonal_stats import *
 
 #%%
-def run_updates(adv_year: int = 0, adv_pages: int = 0, all_adv_pages: bool = False, trad_year: int = 0, trad_pages: int = 0, all_trad_pages: bool = False, seasonal_stats_year: int = 0, get_all_seasonal_stats: bool = False, espn_game_info_year: int = 0, get_all_espn_game_info: bool = False):
+def run_updates(adv_year: int = 0, adv_pages: int = 0, all_adv_pages: bool = False,
+                trad_year: int = 0, trad_pages: int = 0, all_trad_pages: bool = False,
+                seasonal_stats_year: int = 0, get_all_seasonal_stats: bool = False, 
+                espn_game_info_year: int = 0, get_all_espn_game_info: bool = False):
     '''
     This function is meant to be the only piece of code that the user will need to execute. 
 
     As of right now, this function will:
     - gather all advanced/traditional player statistics from NBA.com
-    - build a schedule of all games played, calculate seasonal stats for each player 
-      for each day of the regular season
+    - build a schedule of all games played
+    - calculate seasonal stats for each player for each day of the regular season
     - grab additional game information from ESPN.com.  
     
     In the future, this function will be able to run the entire model for you. I am currently 
